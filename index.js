e9e999540d1347b16ae4625f8260719fa7aa52bf
Shane Thacker <shane@steadymade.com>
2020-09-30 17:29:07 -0500

    Fix development cache bug

--- index.js
+++ index.js
@@ -4,34 +4,41 @@ const mime = require('mime');
 const zlib = require('zlib');
 const { resolve } = require('path');
 
-module.exports = (dir) => {
-  const cache = {};
-  const root = resolve(dir);
-  const filterNonDotFiles = '\/[^\.][^\/]+$';
 
-  const buildAsset = (path) => {
-    // const mtime = fs.statSync(path).mtime.toUTCString();
-    const body = fs.readFileSync(path);
-    const buffer = zlib.gzipSync(body);
-    return {
-      buffer,
-      headers: {
-        'Cache-Control': 'max-age=31536000, public',
-        'Content-Encoding': 'gzip',
-        'Content-Length': buffer.length,
-        'Content-Type': mime.getType(path),
-        'Vary': 'Accept-Encoding'
-        // 'Last-Modified': mtime,
-        // Expires: Mon, 25 Jun 2013 21:31:12 GMT
-      }
-    };
-  }
+const buildAsset = (path, browserCaching = true) => {
+  const body = fs.readFileSync(path);
+  const buffer = zlib.gzipSync(body);
+  return {
+    buffer,
+    headers: {
+      'Cache-Control': browserCaching ? 'max-age=31536000, public' : 'no-cache',
+      'Content-Encoding': 'gzip',
+      'Content-Length': buffer.length,
+      'Content-Type': mime.getType(path),
+      'Vary': 'Accept-Encoding'
+      // 'Last-Modified': fs.statSync(path).mtime.toUTCString(),
+      // Expires: Mon, 25 Jun 2013 21:31:12 GMT
+    }
+  };
+}
+
+const cacheAssets = (root, cache, browserCaching) => {
+  const filterNonDotFiles = '\/[^\.][^\/]+$';
 
   traversy(root, filterNonDotFiles, (path) => {
     const keyRegex = new RegExp(`^${root}`);
     const key = path.replace(keyRegex, '');
-    cache[key] = buildAsset(path);
+    cache[key] = buildAsset(path, browserCaching);
   });
+}
+
+module.exports = (dir, wait = 0) => {
+  const cache = {};
+  const root = resolve(dir);
+
+  wait
+    ? setTimeout(cacheAssets.bind(null, root, cache, false), wait)
+    : cacheAssets(root, cache, true);
 
   return (req, res, next) => {
     const asset = cache[req.pathname];
@@ -40,7 +47,6 @@ module.exports = (dir) => {
       asset.headers.Date = new Date().toUTCString();
       res.writeHead(200, asset.headers);
       res.end(asset.buffer);
-      // fs.createReadStream(join(root, req.pathname)).pipe(res);
     } else {
       next();
     }
