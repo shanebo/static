b9a7228e3539b61625d7af6ff4a8283545f29a32
Shane Thacker <shane@steadymade.com>
2020-10-02 12:20:43 -0500

    Add cacheControl option and buildWait back in

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ const zlib = require('zlib');
 const { resolve } = require('path');
 
 
-const buildAsset = (path, browserCaching = true) => {
+const buildAsset = (path, cacheControl) => {
   const body = fs.readFileSync(path);
   const buffer = zlib.gzipSync(body);
   return {
     buffer,
     headers: {
-      'Cache-Control': browserCaching ? 'max-age=31536000, public' : 'no-cache',
+      ...cacheControl && {
+        'Cache-Control': cacheControl
+      },
       'Content-Encoding': 'gzip',
       'Content-Length': buffer.length,
       'Content-Type': mime.getType(path),
@@ -22,23 +24,23 @@ const buildAsset = (path, browserCaching = true) => {
   };
 }
 
-const cacheAssets = (root, cache, browserCaching) => {
+const cacheAssets = (root, cache, cacheControl) => {
   const filterNonDotFiles = '\/[^\.][^\/]+$';
 
   traversy(root, filterNonDotFiles, (path) => {
     const keyRegex = new RegExp(`^${root}`);
     const key = path.replace(keyRegex, '');
-    cache[key] = buildAsset(path, browserCaching);
+    cache[key] = buildAsset(path, cacheControl);
   });
 }
 
-module.exports = (dir, wait = 0) => {
+module.exports = (dir, cacheControl, buildWait = 0) => {
   const cache = {};
   const root = resolve(dir);
 
-  wait
-    ? setTimeout(cacheAssets.bind(null, root, cache, false), wait)
-    : cacheAssets(root, cache, true);
+  buildWait
+    ? setTimeout(cacheAssets.bind(null, root, cache, cacheControl), buildWait)
+    : cacheAssets(root, cache, cacheControl);
 
   return (req, res, next) => {
     const asset = cache[req.pathname];
@@ -52,51 +54,3 @@ module.exports = (dir, wait = 0) => {
     }
   }
 }
-
-
-// const { resolve, join } = require('path');
-
-// module.exports = (dir, cacheOn = true) => {
-//   const cache = {};
-//   const root = resolve(dir);
-//   const filterNonDotFiles = '\/[^\.][^\/]+$';
-
-//   if (cacheOn) {
-//     traversy(root, filterNonDotFiles, (path) => {
-//       const keyRegex = new RegExp(`^${root}`);
-//       const key = path.replace(keyRegex, '');
-//       cache[key] = buildAsset(path);
-//     });
-//   }
-
-//   return (req, res, next) => {
-//     const asset = cache[req.pathname];
-
-//     if (req.method !== 'GET' || req.pathname === '/') {
-//       next();
-//       return;
-//     }
-
-//     if (asset) {
-//       asset.headers.Date = new Date().toUTCString();
-//       res.writeHead(200, asset.headers);
-//       res.end(asset.buffer);
-//     } else if (!cacheOn) {
-//       const filePath = join(root, req.pathname);
-
-//       if (fs.existsSync(filePath)) {
-//         res
-//           .status(200)
-//           .set('Cache-Control', 'no-cache')
-//           .set('Date', new Date().toUTCString())
-//           .set('Content-Type', mime.getType(req.pathname));
-
-//         fs.createReadStream(filePath).pipe(res);
-//       } else {
-//         next();
-//       }
-//     } else {
-//       next();
-//     }
-//   }
-// }
